55e85c6e541512727ebdebac6638159100a9dcde
tanmayk1212 <98088854+tanmayk1212@users.noreply.github.com>
2022-04-28 21:43:17 +0530

    Delete spring15-bootx-mvc-JPA-REST_api-webServices directory

--- spring15-bootx-mvc-JPA-REST_api-webServices/src/main/java/com/dac/JPA/REST/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dac.JPA.REST.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dac.JPA.REST.entites.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
